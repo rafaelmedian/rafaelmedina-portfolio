f5d909a78f6a8130ec8a7de35da0fe27a0f42267
zxhadow <rafaelmedina@outlook.com>
2014-12-31 21:53:44 +0000

    Building out Wordpress navigation

--- functions.php
+++ functions.php
@@ -1,4 +1,16 @@
 <?php
+add_theme_support('menus');
+
+function register_nav_theme_menus(){
+	register_nav_menus(
+
+		array (
+			'primary-menu' => __( 'Primary Menu' )
+		)
+	);
+}
+
+add_action( 'init','register_nav_theme_menus' );
 
 function wpt_theme_styles() {
 	wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css'  );
@@ -10,7 +22,7 @@ add_action('wp_enqueue_scripts', 'wpt_theme_styles');
 
 function wpt_theme_js() {
 	wp_enqueue_script('modernizr_js' , get_template_directory_uri() . '/js/modernizr.js' , '' , '', false);
-	wp_enqueue_script('foundation_js' , get_template_directory_uri() . '/js/foundation.js' , array('jquery') , '', true);
+	wp_enqueue_script('foundation_js' , get_template_directory_uri() . '/js/foundation.min.js' , array('jquery') , '', true);
 	wp_enqueue_script('main_js' , get_template_directory_uri() . '/js/app.js' , array('jquery', 'foundation_js') , '', false);
 }
 add_action('wp_enqueue_scripts', 'wpt_theme_js');
